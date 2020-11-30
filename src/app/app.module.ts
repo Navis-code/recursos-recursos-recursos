e5a1d8fa6b14d9e23b7730bb439be2cffbb1a475
Navis-code <info@naviscode.com>
2020-11-30 07:54:31 +0100

    Install swiper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarMenuComponent } from '@components/navbar-menu/navbar-menu.component';
 import { ResourcesWidgetComponent } from '@components/resources-widget/resources-widget.component';
+import { NavbarMenuComponent } from '@components/navbar-menu/navbar-menu.component';
 import { ResourcesCategoryComponent } from './components/resources-category/resources-category.component';
 import { NotFoundComponentComponent } from './components/not-found-component/not-found-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,6 +17,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { ResourceTemplateComponent } from './components/resource-template/resource-template.component';
+// ? SWIPPER
+import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
 
 @NgModule({
   declarations: [
@@ -38,6 +40,7 @@ import { ResourceTemplateComponent } from './components/resource-template/resour
     MatIconModule,
     MatListModule,
     MatCardModule,
+    NgxUsefulSwiperModule,
   ],
   providers: [ResourceService],
   bootstrap: [AppComponent],
