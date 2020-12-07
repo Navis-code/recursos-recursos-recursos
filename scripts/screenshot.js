4a3f659d365818b8e56b80d6f49e96da82e91f3b
Navis-code <info@naviscode.com>
2020-12-07 08:55:08 +0100

    add script img:add and fix not folder found ENOENT err

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -3,11 +3,22 @@ const puppeteer = require("puppeteer");
 
 const imagemin = require("imagemin");
 const imageminPngquant = require("imagemin-pngquant");
+const { statSync, mkdirSync } = require("fs");
+const { join } = require("path");
+
+const FOLDER_PATH = "/src/assets/screenshots";
+/*
+## Comprueba directorio si existe según FOLDER_PATH
+## Si no está lo crea con mkdir
+## Irá ejecutando screenshots correspondiente si no da ningún error (revisar url de JSON)
+## Realizará la compresión de las imagenes
+## Finalizará el proceso
+*/
 
 const compressImg = async () => {
   console.log("\nCompressing images...");
-  const files = await imagemin(["../src/assets/screenshots/*.{jpg,png}"], {
-    destination: "../src/assets/screenshots",
+  const files = await imagemin(["." + FOLDER_PATH + "/*.{jpg,png}"], {
+    destination: "." + FOLDER_PATH,
     plugins: [
       imageminPngquant({
         quality: [0.6, 0.8],
@@ -16,19 +27,21 @@ const compressImg = async () => {
   });
 
   console.log(files);
+  console.log("End compressing images");
 };
 
-const capture = async ({ name, url }) => {
+const capture = async ({ name, url }, screenshotsFolder) => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
     await page.setViewport({ width: 1024, height: 768 });
     await page.screenshot({
-      path: `../src/assets/screenshots/${name}.png`,
+      path: `${screenshotsFolder}/${name}.png`,
     });
     await browser.close();
   } catch (error) {
+    console.error(error);
     console.error(
       `\n ❌ Screenshot fallida nombre: ${name} ## Revisar url: ${url}`
     );
@@ -37,14 +50,21 @@ const capture = async ({ name, url }) => {
 };
 
 async function makeScreenshots(resources) {
+  console.log("Check directory...");
+  const screenshotsFolder = join(__dirname, "..", FOLDER_PATH);
+  if (!(await folderExists(screenshotsFolder))) {
+    mkdirSync(screenshotsFolder, {
+      recursive: true,
+    });
+  }
   console.log("Creating screenshots...");
   for (let index = 0; index < resources.length; index++) {
     const resource = resources[index];
-    await capture(resource);
+    await capture(resource, screenshotsFolder);
     writeProgressBarOnConsole(index, resources.length);
   }
   // RESIZE IMG
-  await compressImg();
+  await compressImg(screenshotsFolder);
   process.exit();
 }
 
@@ -55,6 +75,16 @@ function writeProgressBarOnConsole(index, length) {
   process.stdout.write(
     `\r[${dots}${empty}] ${Math.round(((index + 1) * 100) / length)}%`
   );
+}
+
+async function folderExists(folder) {
+  try {
+    statSync(folder);
+    return true;
+  } catch (err) {
+    if (err.code === "ENOENT") return false;
+    throw err;
+  }
 }
 
 makeScreenshots(resourcesJSON);
